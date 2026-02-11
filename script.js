f6713eac1f620d62ede8e544354b0465cf27278c
Rahul Kr <rahulkumar8604745@gmail.com>
2026-02-11 10:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //your JS code here. If required.
 for(let i = 1 ; i <= 20 ; i++){
 	if(i % 3 === 0){
-		// alert("Fizz")
+		alert("Fizz")
+	}else if(i % 5 === 0){
+		alert("Buzz")
+	}else if(i % 3 === 0 && i % 5 === 0){
+		alert("FizzBuzz")
 	}
 }
